aa058b4efd758a816241b29b791118ec13e997c1
Samuel Dushime <127239702+Samenergy@users.noreply.github.com>
2023-08-13 21:53:18 +0200

    Update features.py

--- why_edu/features.py
+++ why_edu/features.py
@@ -1,10 +1,1 @@
 import json
-
-def get_quotes():
-    with open('data/quotes.json', 'r') as file:
-        return json.load(file)
-
-def get_testimonials():
-    with open('data/testimonials.json', 'r') as file:
-        return json.load(file)
-
