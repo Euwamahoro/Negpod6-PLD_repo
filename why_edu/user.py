7839b3b9ce4cc7ed640b009252cd53b6e4832bc5
Vaste <125889522+vuwase@users.noreply.github.com>
2023-08-13 22:00:16 +0200

    Update user.py

--- why_edu/user.py
+++ why_edu/user.py
@@ -13,3 +13,12 @@ def login(username, password):
     if username in users and users[username]['password'] == password:
         return True
     return False
+    
+def load_users():
+    try:
+        with open('data/users.json', 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}  # Return an empty dictionary if the file is missing or empty
+
+
