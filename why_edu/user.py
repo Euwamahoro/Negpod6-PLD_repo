fe48dc8535b285a094603f769aa248ed8a97a1c6
Vaste <125889522+vuwase@users.noreply.github.com>
2023-08-13 21:58:22 +0200

    Update user.py

--- why_edu/user.py
+++ why_edu/user.py
@@ -7,21 +7,3 @@ def register(username, password):
         save_users(users)
         return True
     return False
-
-def login(username, password):
-    users = load_users()
-    if username in users and users[username]['password'] == password:
-        return True
-    return False
-
-def load_users():
-    try:
-        with open('data/users.json', 'r') as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return {}  # Return an empty dictionary if the file is missing or empty
-
-
-def save_users(users):
-    with open('data/users.json', 'w') as file:
-        json.dump(users, file)
