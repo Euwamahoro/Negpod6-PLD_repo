05d28d02a908410dcfc4d6314d9e354a35a8102a
Latjor Wuon Lat <127384804+Latjor-Wuon@users.noreply.github.com>
2023-08-13 22:15:00 +0200

    Update main.py

--- why_edu/main.py
+++ why_edu/main.py
@@ -11,6 +11,15 @@ def register_user():
         print("Registration successful!")
     else:
         print("Username already exists.")
+def login_user(video_data):
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if user.login(username, password):
+        print("Login successful!")
+        user_features(username, video_data)
+    else:
+        print("Login failed. Incorrect username or password.")
+
 
 
 
